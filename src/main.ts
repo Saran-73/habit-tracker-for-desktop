753dc2609eec1c89ff7deb6de744ba784c70c69e
saran-73 <saransamikannu@gmail.com>
2023-03-09 19:40:20 +0530

    create next or prev month container to navigate to next and prev months

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,86 @@ import { createMonthContainer } from "./scripts/month-board";
 
 // first script that loads right after the dom is loaded
 window.addEventListener("DOMContentLoaded", () => {
-  const totalDays: number = 31;
+  const displayedMonth = document.querySelector(".displayed-month");
+  const displayedYear = document.querySelector(".displayed-year");
+  const incrementBtn = document.getElementById("increment");
+  const decrementBtn = document.getElementById('decrement');
+
+  const monthNames = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+
+  let year: number;
+  let month: number;
+
+  const date = new Date();
+  month = date.getMonth() + 1;
+  year = date.getFullYear();
+
+  displayedMonth.textContent = monthNames[month - 1];
+  displayedYear.textContent = year.toString();
+  
+  const getDays = (year: number, month: number) => {
+    return new Date(year , month, 0).getDate()
+  }
+  const totalDays: number = getDays(year, month);
+
+
+  incrementBtn?.addEventListener('click', handleDateIncrement)
+  decrementBtn?.addEventListener('click', handleDateDecrement)
+
+  function handleDateDecrement() {
+    year--;
+    month--;
+
+    const totalDays: number = getDays(year, month);
+
+    createMonthContainer(totalDays)
+
+  }
+
+  function handleDateIncrement() {
+    year++;
+    month++;
+
+    const totalDays: number = getDays(year, month);
+
+    createMonthContainer(totalDays, true)
+  }
+
+  // create UI
   createMonthContainer(totalDays)
+  
+
+
+  // class Habit{
+  //   name: string;
+  //   alert;
+  //   constructor(name: string) {
+  //     this.name = name;
+  //     this.alert =()=>{
+  //       console.log("alert happened")
+  //     }
+  //   }
+  //   renameHabit(newName: string){
+  //     this.name = newName;
+  //   }
+  // }
+
+  // const a:{ name: string, alert():void}[] = [];
+
+  // const football = new Habit('football');
+  // const read = new Habit('read');
+
+
+  // football.renameHabit('soccer')
+
+
+  // a.push(football)
+  // a.push(read)
+
+
+
 });
 
--- src/scripts/month-board.ts
+++ src/scripts/month-board.ts
@@ -1,15 +1,40 @@
 import { createElement } from "../utils/dom-methods";
 
 
-export const createMonthContainer = (totalDays: number) => {
-    const MONTH_CONTAINER = document.querySelector(".month-container")
+export const createMonthContainer = (totalDays: number, nextMonth?: boolean) => {
+    const MONTH_CONTAINER_WRAPPER = document.querySelector(".month-parent-container");
 
-    for (let i = 0; i < totalDays; i++) {
+    const MONTH_CONTAINER = createElement('section');
+    MONTH_CONTAINER.setAttribute("class", "month-container");
+ 
+    let days = totalDays;
+    // if the month is february just add an extra block but hide that one 
+    if (totalDays === 28) {
+        days = 29
+    }
+
+    for (let i = 0; i < days; i++) {
         const checkmarkDiv = createElement("div");
-        checkmarkDiv.setAttribute("class", "checkmark-block")
+        if (totalDays === 28 && i === 28) {
+            checkmarkDiv.setAttribute("class", "checkmark-block-hidden")
+        } else {
+            checkmarkDiv.setAttribute("class", "checkmark-block")
+        }
         checkmarkDiv.addEventListener("click", handleIsChecked)
         MONTH_CONTAINER?.append(checkmarkDiv);
     }
+
+
+    if (nextMonth) {
+        MONTH_CONTAINER_WRAPPER?.append(MONTH_CONTAINER)   
+        MONTH_CONTAINER.scrollIntoView({block: "start", behavior: "smooth"})
+
+    } else {
+        MONTH_CONTAINER_WRAPPER?.prepend(MONTH_CONTAINER)
+        MONTH_CONTAINER.scrollIntoView({block: "nearest", behavior: "smooth"})
+    }
+
+
 }
 
 const handleIsChecked = (element: any) => {
