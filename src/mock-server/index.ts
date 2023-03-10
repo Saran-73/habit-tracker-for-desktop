6b6d24fcf04d9f3a83161e785279df224da19a46
saran-73 <saransamikannu@gmail.com>
2023-03-10 18:57:22 +0530

    created mock server to all habit and indivdual habit on interacting

--- src/mock-server/index.ts
+++ src/mock-server/index.ts
@@ -1,31 +1,34 @@
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 
 const initializeMockServer = () => {
   createServer({
+    models: {
+      habits: Model,
+    },
+    
     routes() {
       this.namespace = "api";
 
-      this.get("/habits", () => {
-        return {
-          habits: [
-            { id: 1, name: "Read" },
-            { id: 2, name: "Drink more water"},
-            { id: 3, name: "Do yoga" },
-          ],
-        };
+      this.get("/habits", (schema, request) => {
+        return schema.habits.all()
       });
 
-      this.get("/habits/:id", () => {
-        return {
-          habit: {
-            name: "Read",
-            month_data: {
-              march: [ 3, 8 , 9 , 10 ]
-            }
-          }
-        }
+      this.get("/habits/:id", (schema, request) => {
+        let id = request.params.id;
+        return schema.habits.find(id);
       })
+      this.patch("/habits/:id", (schema, request) => {
+        let updatedProp = JSON.parse(request.requestBody);
+        let id = request.params.id;
+        let habit = schema.habits.find(id);
+        return habit.update(updatedProp);
+      })
+    },
 
+    seeds(server) {
+      server.create('habit', {id: "1", name: 'Drink', month_data: [1, 3, 5]})
+      server.create('habit', {id: "2", name: 'Yoga', month_data: [10, 23, 28]})
+      server.create('habit', {id: "3", name: 'Read', month_data: [12, 13, 14]})
     },
   });
 };
--- src/scripts/habit-list.ts
+++ src/scripts/habit-list.ts
@@ -9,15 +9,17 @@ export const createHabitLists = () => {
             .then((response) => response.json())
             .then((data) => {
             createHabitsUI(data); 
+              console.log(data)
           }).catch((error) => {
             console.error(error)
           });
     
-    const createHabitsUI = ({ habits }: { habits: { id: number, name: string }[] }) => {
+    const createHabitsUI = ({ habits }: { habits: { id: string, name: string }[] }) => {
     const SIDEBAR = document.querySelector(".sidebar") 
         habits.map(eachhabit => {
             const EACHHABIT = createElement("div");
             EACHHABIT.setAttribute("class", 'each-habit')
+            EACHHABIT.setAttribute("data-id", eachhabit.id)
             EACHHABIT.addEventListener('click',(e)=> handleFetchingHabit(e, eachhabit.name))
             EACHHABIT.textContent = eachhabit.name;
              SIDEBAR?.append(EACHHABIT)
@@ -25,7 +27,8 @@ export const createHabitLists = () => {
         
 
         function handleFetchingHabit(e: any, name: string) {
-            currentHabit.name = name;
+          currentHabit.name = name;
+          let id = parseInt(e.target.getAttribute("data-id"), 10);
             const allHabitsNode = document.querySelectorAll(".each-habit");
 
             allHabitsNode.forEach((each) => {
@@ -35,11 +38,12 @@ export const createHabitLists = () => {
 
 
             // get and display all the habits name in sidebar
-            fetch("/api/habits/2")
+            fetch(`/api/habits/${id}`)
             .then((response) => response.json())
-            .then(({habit}) => {
+              .then(({habits}) => {
+              console.log(habits)
                 removeMonthContainer()
-                createMonthContainer(31, "Mar-2023", habit?.month_data?.march);
+                createMonthContainer(31, "Mar-2023", habits.month_data);
           }).catch((error) => {
             console.error(error)
           });
